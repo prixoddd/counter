b9cb8b0205df38aff7a6e1bf30c9710863092bb8
Nikita <70059012+prixoddd@users.noreply.github.com>
2023-04-28 18:21:56 +0300

    v1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import s from './App.module.css'
+import {Scoreboard} from './Components/Scoreboard/Scoreboard';
+import {SbControl} from './Components/SbControll/SbControl';
+
+export type Distype = 'active' | 'disabled' | 'error'
+
 
 function App() {
+    const [startValue, setStartValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(0)
+
+    const [scoreBoard, setScoreBoard] = useState<number>(0)
+
+    const [disabled, setDisabled] = useState<Distype>('disabled')
+
+
+    const incScoreBoard = () => {
+        setScoreBoard(scoreBoard + 1)
+    }
+
+    const resetScoreBoard = () => {
+        setScoreBoard(startValue)
+    }
+
+    const controlMaxValue = (n:number) => {
+       setMaxValue(n)
+
+    }
+
+    const controlStartValue = (n: number) => {
+        setScoreBoard(n)
+        setStartValue(n)
+    }
+
+    const setScore = (s:Distype) => {
+        setDisabled(s)
+    }
+
+   // if(maxValue < startValue ) {
+   //     setScore('error')
+   // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className={s.content}>
+        <SbControl controlMaxValue={controlMaxValue}
+                   controlStartValue={controlStartValue}
+                   setScore={setScore}
+                   // checkForError={checkForError}
+        />
+        <Scoreboard scoreBoard={scoreBoard}
+                    resetScoreBoard={resetScoreBoard}
+                    incScoreBoard={incScoreBoard}
+                    maxValue={maxValue}
+                    disabled={disabled}
+        />
+      </div>
+
+  )
 }
 
 export default App;
